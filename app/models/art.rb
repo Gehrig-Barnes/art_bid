1bfdccc77e54f4369c1eb97f84ac480ba6c83cd5
gehrig <gehrigbarnes@gmail.com>
2022-10-29 12:26:27 -0500

    created my routes and relationships

--- app/models/art.rb
+++ app/models/art.rb
@@ -1,3 +1,6 @@
 class Art < ApplicationRecord
   belongs_to :artist
+  has_many :reviews
+  has_many :bids
+  has_many :users, through: :bids
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,19 @@ Rails.application.routes.draw do
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #routes for user login/signup
+  post "/signup", to: "users#create_user"
+  get "/user", to: "users#get_user"
+
+  post "/user_login", to: "sessions#user_login"
+  delete "/user_logout", to: "sessions#user_logout"
+
+  #routes for Artist Login/signup
+  post "/artist_signup", to: "artists#create_user"
+  get "/artist", to: "artists#get_artist"
+
+  post "/artist_login", to: "sessions#artist_login"
+  delete "/user_logout", to: "sessions#user_logout"
+
+  get "*path", to: "fallback#index", constraints: ->(req) {!req.xhr? && req.format.html?}
 end
